286ee9514d5588057366c3bec6efb6013352000b
Namehacked <153319486+Namehacked@users.noreply.github.com>
2023-12-08 13:36:30 -0600

    Update MissileCommand.java

--- src/advancedGraphics/MissileCommand.java
+++ src/advancedGraphics/MissileCommand.java
@@ -18,16 +18,27 @@ public class MissileCommand extends JPanel implements Runnable,MouseListener,Mou
 private int mouse_x,mouse_y;
 
 private ArrayList<missiles> ar;
-
+private Satellite woah;
+private ArrayList<CounterMissile> cmis;
+private ArrayList<cities> cities;	
+private int[] houseX,houseY;
 	public MissileCommand ()
 	{
 		mouse_x=0;
 		mouse_y=0;
 		setBackground(Color.WHITE);
 		ar=new ArrayList<missiles>();
+		cities=new ArrayList<>();
+		houseX= new int[] {200, 350, 450, 750, 900, 1000};
+		houseY= new int[] {500, 500, 500, 490, 490, 490};
+		for(int x=0;x<6;x++){
+			cities.add(new cities(houseX[x], houseY[x],100,50));
+		}
 		for(int z=0; z<30;z++){
 			ar.add(new missiles(0,0,0,2,2,0,0,false));
 		}
+		cmis=new ArrayList<>();
+		woah = new Satellite(20, 70, 50, 50, 6);
 		for(int x=0;x<10;x++){
 			ar.get(x).setX((int)(Math.random()*1200));
 			ar.get(x).setY(100);
@@ -64,7 +75,12 @@ private ArrayList<missiles> ar;
 			cities city= new cities(houseX[x], houseY[x]);
 			city.paint(window);
 		}
-		
+		for(CounterMissile c:cmis){
+			c.paint(window);
+			c.move();
+		}
+		woah.paint(window);
+		woah.move();
 		for(missiles m: ar){
 			if(m.isShow()){
 				m.paint(window);
