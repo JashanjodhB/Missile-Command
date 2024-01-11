ba74eb0ccb047a76f94b65770325cba93cee2c3b
Namehacked <153319486+Namehacked@users.noreply.github.com>
2024-01-11 13:09:58 -0600

    Update cities.java

--- src/advancedGraphics/cities.java
+++ src/advancedGraphics/cities.java
@@ -7,16 +7,16 @@ import javax.swing.*;
 import java.util.*;
 import java.awt.MouseInfo;
 import java.awt.Toolkit;
-public class cities extends JPanel {
-    private int x,y;
-    public cities(int ex,int ey){
-        x=ex;
-        y=ey;
+public class cities extends missiles {
+    public cities(int ex,int ey,int we,int he){
+        super(ex, ey,we,he);
     }
+
+
     public void paint(Graphics window){
         Graphics2D g2= (Graphics2D) window;
         Image citi=Toolkit.getDefaultToolkit().getImage("Cities.png"); 
-        g2.drawImage(citi,x-50,y,100,50,this);
+        g2.drawImage(citi,getX()-50,getY(),getW(),getH(),this);
     }
     
 }
