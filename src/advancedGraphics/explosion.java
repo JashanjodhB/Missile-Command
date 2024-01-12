77cf099b46e000c8f7ba8098e944e67a28776dbd
JashanjodhB <152323133+JashanjodhB@users.noreply.github.com>
2024-01-11 18:00:22 -0600

    Update explosion.java

--- src/advancedGraphics/explosion.java
+++ src/advancedGraphics/explosion.java
@@ -3,7 +3,6 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Color;
-
 import javax.management.timer.Timer;
 import javax.swing.*;
 import java.util.*;
@@ -18,9 +17,7 @@ public class explosion extends JPanel {
         y=ey;
         show=sh;
     }
-    
     public void paintComponent(Graphics window){
-        
         if(show){
             window.setColor(Color.ORANGE);
             window.fillOval(x-30,y-30,60,60);
