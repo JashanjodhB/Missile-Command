ebc6d2cb5d1d48e46964a69fbd9147d7b8371061
JashanjodhB <152323133+JashanjodhB@users.noreply.github.com>
2024-01-18 13:30:42 -0600

    Update CounterMissile.java

--- src/advancedGraphics/CounterMissile.java
+++ src/advancedGraphics/CounterMissile.java
@@ -6,10 +6,11 @@ import java.awt.Color;
 import javax.swing.*;
 import java.util.*;
 import java.awt.MouseInfo;
+import java.awt.Rectangle;
 import java.awt.Toolkit;
 public class CounterMissile extends missiles{
     private int vertVelocity,mouseX,mouseY,horzVelocity;
-    private boolean left;
+    private boolean left,explo;
     public CounterMissile(int v,int xe, int ye,int we, int he,int mx,int my,boolean tr) {
         super(xe, ye,we,he);
         mouseX=mx;
@@ -21,6 +22,7 @@ public class CounterMissile extends missiles{
             left=true;
         }
         setShow(tr);
+        explo=true;
 
     }
     public void paint(Graphics window){
@@ -31,6 +33,23 @@ public class CounterMissile extends missiles{
         setX(getX()+horzVelocity);
         setY(getY()+vertVelocity);
     }
+    public boolean intersects( missiles other ) {
+        if(explo){
+            Rectangle one = new Rectangle(getX(), getY() , 90,90);
+            Rectangle two = new Rectangle(other.getX(),other.getY(),other.getW(),other.getH());
+            if (one.intersects(two)) {
+               explo=false;
+                return true;
+               
+            }
+            return false;
+        }
+        return false;
+        
+      }
+      public void expShow(boolean s){
+        explo=s;
+      }
     public void IsInBounds(Graphics window){
         if(left && getX()<mouseX){
             show=false;
