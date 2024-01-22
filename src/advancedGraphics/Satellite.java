5547cba01664784d01fa98071921b8ef7e4f6d67
Namehacked <153319486+Namehacked@users.noreply.github.com>
2024-01-22 13:15:21 -0600

    Update Satellite.java

--- src/advancedGraphics/Satellite.java
+++ src/advancedGraphics/Satellite.java
@@ -8,21 +8,24 @@ import javax.swing.*;
 import java.util.*;
 import java.awt.MouseInfo;
 import java.awt.Toolkit;
-public class Satellite extends JPanel{
-    private int x,y,w,h,v;
-    public Satellite(int ex,int ey,int ew,int eh, int velocity){
-        x=ex;
-        y=ey;
-        w=ew;
-        h=eh;
+public class Satellite extends missiles{
+    private int v;
+    public Satellite(int ex,int ey,int ew,int eh, int velocity,boolean sh){
+        super(ex,ey,ew,eh);
         v=velocity;
+        setShow(sh);
     }
     public void paint(Graphics window){
         Graphics2D g2= (Graphics2D) window;
         Image satellite= Toolkit.getDefaultToolkit().getImage("Satellite.png");
-        g2.drawImage(satellite,x,y,w,h,this);
+        g2.drawImage(satellite,getX(),getY(),getW(),getH(),this);
     }
     public void move() {
-        x+=v;
+        setX(getX()+v);
+    }
+    public void inBounds(Graphics window){
+        if(getX()>1200){
+            setShow(false);
+        }
     }
 }
